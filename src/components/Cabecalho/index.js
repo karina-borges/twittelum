522e2db0039bef656be1a062e91b595045ce84a1
Karina Borges <karina_borges@id.uff.br>
2021-06-16 10:06:33 -0300

    ✨ First commit

---
+++ src/components/Cabecalho/index.js
@@ -0,0 +1,16 @@
+import cabecalhoStyles from './cabecalho.module.css'
+
+function Cabecalho( { children } ) {
+    return (
+        <header className={cabecalhoStyles.cabecalho}>
+            <div className={`${cabecalhoStyles.cabecalho__container} container`}>
+                <h1 className={cabecalhoStyles.cabecalho__logo}>
+                    <a href="/">Twitelum</a>
+                </h1>
+                { children }
+            </div>
+        </header>
+    )
+}
+
+export default Cabecalho
